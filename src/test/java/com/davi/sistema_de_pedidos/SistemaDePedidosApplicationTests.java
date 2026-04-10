6446c31edc79b423766b65e6196364980de7896a
Davi <daviaika07@gmail.com>
2026-04-10 18:42:47 -0300

    Initial commit

---
+++ src/test/java/com/davi/sistema_de_pedidos/SistemaDePedidosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.davi.sistema_de_pedidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDePedidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
